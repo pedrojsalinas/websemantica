c4e2c1cfb6b0a279366d7dd719b41e2e09ac4b8c
Pedro Salinas <pedrosalinas5@gmail.com>
2019-07-18 18:45:35 -0500

    correcion metodos

--- sbc/views.py
+++ sbc/views.py
@@ -8,6 +8,7 @@ from rdflib.serializer import Serializer
 from .forms import SbcForm
 from collections import OrderedDict
 import itertools
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 class IndexView(TemplateView):
     '''Metodo que renderiza la plantilla index.html'''
@@ -25,10 +26,16 @@ class IndexView(TemplateView):
             text = form.cleaned_data['consulta']
             # asigan texto inicial a variable
             initTexto = text
-            token = Tokenizador()
-            datos = token.limpiezaDatos(text)
+            print('---'*10)
+            print(initTexto)
+            print('---'*10)
+            # token = Tokenizador()
+            semantico = Semantico()
+            # datos = token.limpiezaDatos(text)
+            datosTest = semantico.consultaVirutoso(text)
+            # print(datosTest)
             form = SbcForm()
-            args = {"datos": datos, "form": form, "texto": initTexto}
+            args = {"datos": datosTest, "form": form, "texto": initTexto}
         return render(request, self.template_name, args)
 
 
@@ -41,7 +48,7 @@ class Tokenizador():
         tokenized_sentences = [sentence.text for sentence in text.sents]
         g = rdflib.Graph()
         # nombre del archivo
-        g.parse("datos-2.rdf")
+        g.parse("mydataset.rdf")
         datos = []
 
         for sentence in tokenized_sentences:
@@ -64,3 +71,48 @@ class Tokenizador():
         for i in datos:
             lista.append(i)
         return lista
+
+
+class Semantico():
+    sbcEndpoint = SPARQLWrapper("http://localhost:8890/sparql/")
+    nlp = es_core_news_sm.load()
+
+    def consultaVirutoso(self, texto):
+        text = self.nlp(texto)
+        tokenized_sentences = [sentence.text for sentence in text.sents]
+        datos = []
+        for sentence in tokenized_sentences:
+            for entity in self.nlp(sentence).ents:
+                consulta = """
+                SELECT ?s ?p ?o
+                    WHERE 
+                        { 
+                            ?s ?p ?o .FILTER regex(str(?s), "%s") .
+                        }
+                        """ % (entity.text)
+                self.sbcEndpoint.setQuery(consulta)
+                self.sbcEndpoint.setReturnFormat(JSON)
+                results = self.sbcEndpoint.query().convert()
+                for result in results["results"]["bindings"]:
+                    lista = []
+                    listaS = result["s"]["value"]
+                    listaP = result["p"]["value"]
+                    listaO = result["o"]["value"]
+                    lista.append(listaS)
+                    lista.append(listaP)
+                    lista.append(listaO)
+                    datos.append(lista)
+        return datos
+
+    def consultaPorUri(self, uri):
+        consulta = """
+                    SELECT ?p ?o
+                        WHERE
+                        {
+                            <%s> ?p  ?o
+                        }
+                """ % (uri)
+        self.sbcEndpoint.setQuery(consulta)
+        self.sbcEndpoint.setReturnFormat(JSON)
+        results = self.sbcEndpoint.query().convert()
+        return results["results"]["bindings"]
